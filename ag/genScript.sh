791cb4a5e0aeb3324146fad7b4850e5bc668c6c5
giucapo <67631323+giucapo@users.noreply.github.com>
2023-03-20 21:57:08 +0100

    Add files via upload

---
+++ ag/genScript.sh
@@ -0,0 +1,6 @@
+rm -rf output/ && mkdir output
+docker run -ti --name mulval -v "$(pwd)"/output:/input -d --rm wilbercui/mulval bash -c "tail -f /dev/null"
+docker cp scass.pl mulval:/input
+docker cp interaction_rules_def.P mulval:/input
+docker exec mulval bash -c "graph_gen.sh scass.pl -v -p -r interaction_rules_def.P"
+docker stop mulval
